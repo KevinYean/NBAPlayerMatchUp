fbaa7e8e2296f5a27eee58272fadc3632e6509ee
KevinYean <kevin.yean@hotmail.com>
2019-09-20 19:59:34 +0200

    Scraping Finished
    Finished the Scrapping. Everything look s good.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # NBAPlayerMatchUp
  An experiment to  visualize and analyze player match-ups against specific NBA teams throughout their careers.
 
-Currently the plan is a as follow:
+Completed Goals.
 -Scrape all NBA gamelogs from now til 2010 from each active players with more than 10 minutes.
-    -In order for the correctgamelogs to be searched without having to manually search and do more calculations, I will pull the players from my mongodb database with their respective url and have it as a list.
--Scrape all active NBA players who played at least 10 minutes career wise. (Completed)
--Scrape all NBA teams currently active with their abbreviations. (Completed)
+    -In order for the correct gamelogs to be searched without having to manually search and do more calculations, I will pull the players from my mongodb database with their respective url and have it as a list. (Completed, September 20,2019)
+    -Go through each gamelog pages for each season up to 2010 if possible. ("Completed", Forgot to add the 2010 requirement so I scraped all the gamelogs instead, September 20,2019)
+-Scrape all active NBA players who played at least 10 minutes career wise. (Completed,September 19,2019)
+-Scrape all NBA teams currently active with their abbreviations. (Completed,September 19,2019)
+
+Current Goals:
+- Verify the data is correct with some PyTest
+- Verify the correct type of data is there
+- Graph Lebron James points scored against all 30 teams on the x axis with dots or teams respective logos.
--- scraper/scraper/spiders/gamelog_spider.py
+++ scraper/scraper/spiders/gamelog_spider.py
@@ -11,28 +11,60 @@ client = pymongo.MongoClient(uri)
 db = client.NBA_Match_Ups
 
 class GamelogSpider(scrapy.Spider):
-    name = "gamelog"
+    name = "gamelog" #Name of Crawler
     download_delay = 0.5
     currentseason = ""
 
     def start_requests(self):
-        pipeline = [ #MongoDB Pipeline
-                {
-                    '$project': {
-                        'basketballreference_page': 1
-                    }
-                }
-            ]
-        players = db.GameLogs.aggregate(pipeline).next()
+        players = db.Players.find({},{'basketballreference_page':1, "_id": 0}) #Project only the url of basketball reference page
+        listOfUrls = [] #Empty List
+        for document in players: #For loop to go through the entire Cursor
+            listOfUrls.append(document['basketballreference_page']) #Add the url to the list
 
-        urls = [  # List of Urls to go through
-           "https://www.basketball-reference.com/players/l/lillada01.html"
-        ]
+        # List of Urls to go through
+        urls = listOfUrls
+        #urls = [#"https://www.basketball-reference.com/players/l/lillada01/gamelog/2013"]
+         #   "https://www.basketball-reference.com/players/l/lillada01.html"]
         for url in urls:  # Run parse for each url in urls
             yield scrapy.Request(url=url, callback=self.parsePlayers)
     
     def parsePlayers(self,response):
-        yield {
-            "test" : response.url,
-            "second Test" : db.list_collection_names()
-        }
+        links = response.css('ul[class=""] a::attr(href)').getall()
+        links = set(links)
+        for i in links:
+            if "gamelog" in i and "playoffs" not in i: #Only keep gamelogs from the set
+                    yield scrapy.Request(url=response.urljoin(i), callback=self.parseGameLogs)
+
+    def parseGameLogs(self,response):
+        #Player
+        h1 = response.xpath('//h1/text()').get()
+        h1 = h1.split(' ')
+        name = ' '.join(h1[:-3]) #Go Backward
+        #Year/Season
+        year = h1[-3]
+        #Game Number played by player
+        numGamesPlayed = len(response.xpath('//tr[contains(@id,"pgl")]').getall())
+        gameResults = response.xpath('//tr[contains(@id,"pgl")]//td[contains(@data-stat,"game_result")]//text()').getall()
+        #Tm, OP
+        teams = response.xpath('//tr[contains(@id,"pgl")]//td[contains(@data-stat,"team_id")]//text()').getall()
+        opps = response.xpath('//tr[contains(@id,"pgl")]//td[contains(@data-stat,"opp_id")]//text()').getall()
+        #Minutes
+        mp = response.xpath('//tr[contains(@id,"pgl")]//td[contains(@data-stat,"mp")]//text()').getall()
+        #Field Goal %       
+        fg_pct = response.xpath('//tr[contains(@id,"pgl")]//td[contains(@data-stat,"fg_pct")]//text()').getall()
+        #Points
+        pts = response.xpath('//tr[contains(@id,"pgl")]//td[contains(@data-stat,"pts")]//text()').getall()
+        #For each Yield
+        for i in range (numGamesPlayed):
+            yield{
+                "name" : name,
+                "year" : year,
+                "game" : i+1,
+                "team" : teams[i],
+                "opponent" : opps[i],
+                "result" : gameResults[i],
+                "points" : int(pts[i]),
+                "minutes" : mp[i],
+                "fg_pct" : float(fg_pct[i])
+            }
+
